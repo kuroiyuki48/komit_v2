22ce5d2aeda6c70986d9dc897332db7231ff2ce7
NoorAlizah <nooralizah25@gmail.com>
2019-10-20 10:03:36 +0700

    update index.html

---
+++ js/front.js
@@ -0,0 +1,51 @@
+$(function () {
+
+    // ------------------------------------------------------- //
+    // Navbar Sticky
+    // ------------------------------------------------------ //
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > ($('.top-bar').outerHeight())) {
+            $('header.nav-holder.make-sticky').addClass('sticky');
+            $('body').css('padding-top', '' + $('#navbar').outerHeight() + 'px');
+
+        } else {
+            $('header.nav-holder.make-sticky').removeClass('sticky');
+            $('body').css('padding-top', '0');
+        }
+    }); 
+
+    // ------------------------------------------------------- //
+    // Scroll To
+    // ------------------------------------------------------ //
+    $('.scroll-to').on('click', function (e) {
+
+        e.preventDefault();
+        var full_url = this.href;
+        var parts = full_url.split("#");
+        var target = parts[1];
+
+        if ($('header.nav-holder').hasClass('sticky')) {
+            var offset = -80;
+        } else {
+            var offset = -180;
+        }
+
+        var offset = $('header.nav-holder').outerHeight();
+
+        $('body').scrollTo($('#' + target), 800, {
+            offset: -offset
+        });
+
+    });
+
+    // ------------------------------------------------------- //
+    // Adding fade effect to dropdowns
+    // ------------------------------------------------------ //
+    $('.dropdown').on('show.bs.dropdown', function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).fadeIn(100);
+    });
+    $('.dropdown').on('hide.bs.dropdown', function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).fadeOut(100);
+    });
+
+});
